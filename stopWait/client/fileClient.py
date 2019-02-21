10fcb3ca7fb8aa0959334ea59b6fb3a3d0afcd32
student <Sebsatianq1290@gmail.com>
2019-02-21 00:26:17 -0700

    added timeout cutoff to  UDPServerSelect1

---
+++ stopWait/client/fileClient.py
@@ -0,0 +1,15 @@
+student@arch1:~/Desktop/fileTransfer/stopWait/client> ls
+fileClient.py  fileClient.py~  framedClient.py  framedThreadClient.py  README.md  #UDPclient.py#
+student@arch1:~/Desktop/fileTransfer/stopWait/client> ./fileClient.py
+  File "./fileClient.py", line 37
+    if fileName = "quit":
+                ^
+SyntaxError: invalid syntax
+student@arch1:~/Desktop/fileTransfer/stopWait/client> ./fileClient.py
+serverAddr = ('localhost', 50000)
+Input a dummy filename
+t.py
+Modified message from ('127.0.0.1', 50000) is "T.PY"
+Input a dummy filename
+quit
+student@arch1:~/Desktop/fileTransfer/stopWait/client> 
---
+++ stopWait/server/fileServer.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+# udp demo -- simple select-driven uppercase server
+
+# Eric Freudenthal with mods by Adrian Veliz
+
+import sys
+from socket import *
+from select import select
+
+serverAddr = ("", 50000)   # any addr, port 50,000
+
+def uppercase(sock):
+  "run this function when sock has rec'd a message"
+  message, clientAddrPort = sock.recvfrom(2048)
+  print("from %s: rec'd '%s'" % (repr(clientAddrPort), message))
+  modifiedMessage = message.decode().upper().encode()
+  sock.sendto(modifiedMessage, clientAddrPort)
+  
+
+supperServerSocket = socket(AF_INET, SOCK_DGRAM)
+supperServerSocket.bind(serverAddr)
+supperServerSocket.setblocking(False)
+
+# map socket to function to call when socket is....
+readSockFunc = {}               # ready for reading
+writeSockFunc = {}              # ready for writing
+errorSockFunc = {}              # broken
+
+timeout = 3                     #Allocates 3 seconds before failing each connection
+
+# function to call when upperServerSocket is ready for reading
+readSockFunc[supperServerSocket] = uppercase
+
+print("ready to receive")
+timeoutNum = 0
+while 1:
+  readRdySet, writeRdySet, errorRdySet = select(list(readSockFunc.keys()),
+                                                list(writeSockFunc.keys()), 
+                                                list(errorSockFunc.keys()),
+                                                timeout)
+  if not readRdySet and not writeRdySet and not errorRdySet:
+    print("timeout: no events")
+    timeoutNum+=1
+  if timeoutNum == 5:
+    print("Timeout limit reached, disconnecting")
+    break
+  for sock in readRdySet:
+    timeoutNum = 0 
+    readSockFunc[sock](sock)
+
