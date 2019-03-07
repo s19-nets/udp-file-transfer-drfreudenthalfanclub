108dfb0a5f0f45c60c4151fb24e33adbe843f90c
student <Sebsatianq1290@gmail.com>
2019-03-07 16:16:44 -0700

    added select[v1], changed protocol header format

--- stopWait/client/fileClient.py
+++ stopWait/client/fileClient.py
@@ -3,10 +3,12 @@
 
 from socket import *
 import os
-import sys, re                          
-
+import sys, re
+import math
+from select import select
 # default params
-serverAddr = ('localhost', 50000)       
+serverAddr = ('localhost', 50000)
+clientAddr = ("", 50001) #any address but port 50001
 
 def usage():
     print("usage: %s [--serverAddr host:port]"  % sys.argv[0])
@@ -25,24 +27,79 @@ try:
 except:
     usage()
 
-
 print("serverAddr = %s" % repr(serverAddr))
-
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-print("Input file name. Format: foo.extension")
-fileName = sys.stdin.readline()[:-1]     # delete final \n
-filePath = './%s' % (fileName)
-fileExists=os.path.isfile(filePath)
+
+def fileTransfer(sock):
+    print("Input file name. Format: foo.extension")
+    fileName = sys.stdin.readline()[:-1]     # delete final \n
+
+    #should move these into a different method to clean up later
+    filePath = './%s' % (fileName)
+    fileExists=os.path.isfile(filePath)
+    fileSize = os.path.getsize(filePath)
+    totalPackets = str(math.ceil(fileSize/10))
+    filenamePacket = 'f,'+totalPackets+","+fileName
+    print(filenamePacket)
+    
+    if (fileExists):
+        # True, proceed with sending filename (and data in the future)
+        print("file found, or something like that")
+        #can use something like re.split(',') to seperate header/payload
+        #head should include the following
+     
+        clientSocket.sendto(filenamePacket.encode(), serverAddr)
+        fileAck, serverAddrPort = clientSocket.recvfrom(2048)
+        print('fileAck from %s is "%s"' % (repr(serverAddrPort), fileAck.decode()))
+
+    else:
+        print ("file not found, exiting program")
+
+#a socket for incoming connections to us, the client
+supperClientSocket = socket(AF_INET, SOCK_DGRAM)
+supperClientSocket.bind(clientAddr)
+supperClientSocket.setblocking(False)
+
+# map socket to function to call when socket is....
+readSockFunc = {}               # ready for reading
+writeSockFunc = {}              # ready for writing
+errorSockFunc = {}              # broken
+
+timeout = 3                     #Allocates 3 seconds before failing each connection
+
+# function to call when upperServerSocket is ready for reading
+#readSockFunc[supperClientSocket] = fileTransfer
+writeSockFunc[supperClientSocket] = fileTransfer
+
+#print("Type in the file that you would like to send")
+timeoutNum = 0
+while 1:
+  readRdySet, writeRdySet, errorRdySet = select(list(readSockFunc.keys()),
+                                                list(writeSockFunc.keys()), 
+                                                list(errorSockFunc.keys()),
+                                                timeout)
+  
+  if not readRdySet and not writeRdySet and not errorRdySet:
+    print("Client timeout: no events")
+    timeoutNum+=1
+  if timeoutNum == 5:
+    print("Client timeout limit reached, disconnecting")
+    break
+
+  for sock in readRdySet:
+    #would read be needed for this scenario?
+    print("in readySet")
+    #readSockFunc[sock](sock) #(sock) is an arg?
+    timeoutNum = 0
+  for sock in writeRdySet:
+      #change based on what the result was?
+      #or deal with that within the actual method itself?
+      writeSockFunc[sock](sock)
+  for sock in errorRdySet:
+      print("in errorSet")
 
 
-if (fileExists):
-    # True, proceed with sending filename (and data in the future)
-    print("file found, or something like that")
-    clientSocket.sendto(fileName.encode(), serverAddr)
-    fileAck, serverAddrPort = clientSocket.recvfrom(2048)
-    print('fileAck from %s is "%s"' % (repr(serverAddrPort), fileAck.decode()))
 
-else:
-    print ("file not found, exiting program")
-        
-#Do a check here to see if that fileName is valid, if so proceed, if not repeat?
+
+#deprecated code that might be useful later on during development
+#fileSize = os.path.getsize(filePath)
